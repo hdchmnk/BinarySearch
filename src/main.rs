c24ec971b8f5cca624424956a47acc89b8c4136c
klexq <daniilevgaliev@yandex.ru>
2024-04-08 19:49:27 +0300

    binary search

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let arr = [-1, 3, 5, 7, 8, 10, 24, 37, 42, 135];
+
+    let result = bin_search(&arr, 42);
+
+    match result {
+        Some((found_value, found_index)) => println!("Found value {found_value}, Found index {found_index}"),
+        None => println!("Not found!")
+    }
+}
+
+fn bin_search(arr: &[i32], desired_value: i32) -> Option<(i32, usize)> {
+    let mut low_bound: usize = 0;
+    let mut up_bound: usize = arr.len() - 1;
+    let mut i: usize = 0;
+
+    while low_bound <= up_bound {
+        i += 1;
+
+        let mid: usize = (up_bound + low_bound) / 2;
+
+        let mid_value = arr[mid];
+
+        if mid_value == desired_value {
+            return Some((mid_value, mid))
+        } else if desired_value > mid_value {
+            low_bound = mid + 1;
+        } else {
+            up_bound = mid - 1;
+        }
+
+        println!("Step {i}");
+    }
+
+    None
+}
